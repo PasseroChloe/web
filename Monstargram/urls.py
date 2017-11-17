72781afef5279800abfbd65e4c87743354805d67
Passero <passero_23333@163.com>
2017-11-17 15:16:18 +0800

    made some changes in 5 files

--- Monstargram/urls.py
+++ Monstargram/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     url(r'^users/$', views.user_list),
-    url(r'^user/(?P<pk>[0-9]+)/$', views.user_detail),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.user_detail),
     url(r'^resources/$', views.resource_list),
     url(r'^user_comments/$',views.user_comment_list),
     url(r'^user_likes/$', views.user_likes_list),
